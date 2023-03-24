bc2e7fd4914b5806464f81c9cf10c011da2f2ee6
Nataliia Kutueva <kutueva.natalya@yandex.ru>
2023-03-24 20:39:26 +0300

    Сданный вариант

--- main.py
+++ main.py
@@ -18,36 +18,35 @@ if __name__ == '__main__':
     matrix = np.array(matrix_inp)
     transitions = [0 for i in range(size)]
     z = int(input('Введите начальное состояние: '))
-    stoch_state = np.zeros(size)
-    stoch_state[z] = 1
+    stoch_state = np.full(size, 1 / size)
+    #stoch_state = np.zeros(size)
+    #stoch_state[z] = 1
     steps_number = int(input('Введите количество шагов: '))
     for i in range(steps_number):
-        z_values.append(z)
         rand_n = random.random()
         border = 0.0
+        prob_ints = sorted(range(size), key=lambda k: matrix[z][k])
         for j in range(size):
-            if rand_n <= (border + matrix[z][j]):
-                transitions[j] += 1
+            border += matrix[z][prob_ints[j]]
+            if rand_n <= border:
+                transitions[prob_ints[j]] += 1
                 probabilities = []
                 s = sum(transitions)
                 for k in range(size):
                     p = transitions[k] / s
                     probabilities.append(p)
                 states_model_values.append(probabilities)
-                z = j
+                z = prob_ints[j]
+                z_values.append(z)
                 break
-            border += matrix[z][j]
         stoch_state = np.dot(stoch_state, matrix)
         states_theory_values.append(stoch_state)
 
     with open("results3.txt", "w") as f2:
         for i in range(steps_number):
             states_model_formatted = ['%.3f' % elem for elem in states_model_values[i]]
-            # states_model_formatted = np.around(np.array(states_model_values[i]), 3)
-            states_theorie_formatted = np.around(states_theory_values[i], 3)
+            states_model_formatted = "[{0}]".format(', '.join(map(str, states_model_formatted)))
+            np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+            states_theorie_formatted = np.array2string(states_theory_values[i], separator=', ')
             f2.write(str(i) + "\t" + str(z_values[i]) + "\t" + str(states_model_formatted) + "\t" + str(states_theorie_formatted) + "\n")
     f2.close()
-
-
-    # print(matrix)
-
